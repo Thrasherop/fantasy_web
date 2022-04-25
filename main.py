4427a8693bd6ba63808665022c2532fead4e3d05
Thrasherop <ultraprim@gmail.com>
2022-04-25 13:22:17 -0600

    set up file structure

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 # basic flask server
 
 
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify, render_template
+import flask
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template("index.html")#"hello world"
 
-app = Flask(__name__)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5705)
