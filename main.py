d0baa4f4c11740aaac3f0dbb00d809829a28de6e
Thrasherop <ultraprim@gmail.com>
2022-04-25 17:40:52 -0600

    Added dodododo downloads screen and functionality

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # basic flask server
 
 
-from flask import Flask, redirect, request, jsonify, render_template
+from flask import Flask, redirect, request, jsonify, render_template, send_file
 import flask
 
 app = Flask(__name__)
@@ -10,13 +10,19 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/downloads')
-def downloads():
-    return render_template("downloads.html")
+@app.route('/dodododo_downloads')
+def dodododo_downloads():
+    return render_template("dodododo_downloads.html")
+
+@app.route('/dodododo_download_all')
+def dodododo_download_all():
+    return send_file('misc/download_files/ALL-4-worlds.zip', as_attachment=True)
+
 
 @app.route('/github_redirect')
 def github_redirect():
     return redirect("https://github.com/Thrasherop")
+
 
 
 if __name__ == "__main__":
---
+++ static/scripts/dodododo_downloads.js
@@ -0,0 +1,10 @@
+
+
+document.querySelector("#download_all_btn").addEventListener('click', function() {
+
+    window.location.replace("/dodododo_download_all");
+
+});
+
+
+
