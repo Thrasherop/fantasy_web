9ca0be8f0845cb579657cb8421fdd18f2398ef24
Thrasherop <ultraprim@gmail.com>
2022-04-25 18:13:01 -0600

    added stuff

--- static/scripts/dodododo_downloads.js
+++ static/scripts/dodododo_downloads.js
@@ -6,5 +6,9 @@ document.querySelector("#download_all_btn").addEventListener('click', function()
 
 });
 
+document.querySelector("#main_menu_btn").addEventListener('click', function() {
 
+    window.location.replace("/");
+
+});
 
---
+++ static/scripts/index_main.js
@@ -0,0 +1,12 @@
+document.querySelector("#goto_dodododo_downloads").addEventListener('click', function() {
+
+    window.location.replace("/dodododo_downloads");
+
+});
+
+
+document.querySelector("#goto_github").addEventListener('click', function() {
+
+    window.location.replace("https://www.github.com/thrasherop");
+
+});
